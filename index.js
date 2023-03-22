a718d98f3cbd5121da1dc0130becef048b0aeedb
clint360 <clint360.rebase@gmail.com>
2023-03-22 18:20:09 +0100

    login with api key

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const logInWithEmailAndPassword = require('./logInWithEmailAndPassword')
 const relationships = require('./database/relationships'); 
 const regUser = require('./registerUser');
+const logInWithAPIKey = require('./logInWithAPIKey');
+
 
 let user = {
     firstName: 'Che',
@@ -11,5 +13,6 @@ let user = {
     password: '+clint360'
 }
 
-regUser(user);
+//regUser(user);
+logInWithAPIKey('8ea73248-25c6-4e98-be63-cd2f91932d9e').then(console.log)
 //logInWithEmailAndPassword('clint360.rebase@gmail.com', '+Beatzboy360').then(console.log)
--- logInWithEmailAndPassword.js
+++ logInWithEmailAndPassword.js
@@ -1,23 +1,23 @@
 const User = require("./database/user");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_PRIVATE_KEY = 'AXSDFS';
 
- async function logInWithEmailAndPassword(email, password) {
-   const user = await User.findOne({ where: { emailAddress: email } });
-   if (!user) {
-   return {
-      status: 401
-   }; 
-   } else {
-      bcrypt.compare(password, user.password, function(err, result) {
-         if(err) {
-            return {
-               status: 500,
-            }
-         } else {
-            return user
-         }
-     });
-   }
-}
 
-module.exports = logInWithEmailAndPassword
+const loginWithEmailPassword = async (email, password) => {
+  const user = await User.findOne({
+    where: { emailAddress: email },
+  });
+  console.log(user);
+  if (!user) {
+    return { status: 401 };
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return { status: 500 };
+  const token = jwt.sign({ user_id: user.id, email }, JWT_PRIVATE_KEY, {
+    expiresIn: "2h",
+  });
+  return { token, user };
+};
+
+module.exports = { loginWithEmailPassword };
